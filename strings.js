5f066a61d0bf080585abf9e376a2fe909227c5b3
AryanG <aryan1234guptaa@gmail.com>
2025-08-23 00:52:23 +0530

    strings in js update

--- strings.js
+++ strings.js
@@ -81,9 +81,110 @@ function char (string,char){
 
 char('i am a mentor','g')
 
-const pr = (text,char)=> {
-    let index = text.indexOf(char)
-    index<0?"not present":"present"
+let pr = (text, char) => { 
+    let index = text.indexOf(char);
+    return index < 0 ? "not present" : "present";
 }
 
 console.log(pr("heelo","g"))
+
+//includes() -> most useful method in string , checks if a string/char is present in a string or not -> will return a boolean value
+
+msg = "i love code"
+console.log(msg.includes("code"))
+// can be used as condn as retruns a bool
+
+if(msg.includes("dark")){
+    console.log('dark person')
+}
+else
+{
+console.log("light person")
+}
+
+//vowels in a string or not
+let vowels = "aeiou"
+for(let char of msg){
+    if (vowels.includes(char)){
+        console.log(char,'is a vowel')
+    }
+}
+let flag
+
+for(let char of msg){
+    if (vowels.includes(char)){
+        flag = true
+        break;
+    }
+    else{
+        flag=false
+        break;
+    }
+}
+
+if (flag){
+    console.log("vowel present")
+}
+else{
+    console.log("vowel not present")
+}
+
+//touppercase , tolowercase -> converting case of any string
+
+msg ="i am aryann"
+
+console.log(msg.toUpperCase()) // returns a string where every char is converted to upercase
+console.log(msg) //orignal msg not changed
+
+//we can chain multiple methods
+
+for(let char of msg){
+    if (vowels.includes(char.toLowerCase())){ //convterted every char of string to lower case compared to aieou , coversion imp if string contains A as js is case sesitive it will not identified as vowel as not included in aeiou
+        console.log(char, 'is a vowel')
+    }
+}
+
+//substring method -> if a string is too short and u want to display cut short and display some part of it
+
+const  username= "Aryan Gupta"
+let startno=0 //starting char of substring from 0th index
+let endno = 9 //no charcters in substring
+const substring = username.substring(startno,endno) //no of index
+//include charcter from 0th index to 9 index making it 10 charcters
+
+console.log(substring,"...")
+
+//usefull in responsive design if cut short a too long video title in a small screen
+
+
+//trim method -> to cut short and delete extra space
+
+const text = "    hola !! how u    " //extra space on left and right of text u need to delelte as applying other string ,ethods to this may give unxpected o/p , can be a input by user
+
+console.log(text.length) //each space also included in string length
+console.log(text.substring(0,6))
+console.log(text.trim()) //trim removes leading space and trailing spcae from a string
+console.log(text.trim().substring(0,6)) //chaining of methods can be done in some cant in some so see acc.
+
+console.log(msg.length) //retuns the total length of string , total no characters in a string
+
+//u can explore for methods of strings go through that , understand , read through docs .. as covering all methods no possible .
+// done all imp , but u can read more docs and articles for more methods eg: concat ,slice etc..
+//no one remebers all methods from day 1 and no need to -> u will get it when u use regulary and visit and read when u need something
+
+//new way to create string : -> not prefferd much
+let lateststring = new String("hello") // creates a new string hello and stores in variable lateststring
+
+console.log((lateststring)) //prints string in diff way in  object  form
+console.log(typeof lateststring) // creating  something using new keyword creates a object type data typee storing that value only
+//stores string data in object from and also inclues its method 
+// 0:h
+//1:e
+//.
+//.
+//lenght : 5 , u can access all this normally through lateststring[1],.length .. see it in chrome console
+
+
+//when string created in normal way using let str = 'heelo' , memory sllocstion in stack
+// when string creted usong new keyword memroy allocation in heap of data and varibale stored in stack stroting the refrence to this data in heap
+
