3726e853ffa2a8e5fe99bc34a2f4a426cc182852
AryanG <aryan1234guptaa@gmail.com>
2025-08-28 23:47:20 +0530

    update

--- array.js
+++ array.js
@@ -1,1 +1,10 @@
 //Array in js
+const arr = [1,2,3,4,'hello',true]
+console.log(arr)
+console.log(typeof arr)
+
+const arr2 = new Array(1,2,3)
+console.log(arr2)
+console.log(typeof arr2)
+
+console.log(arr.length)
